1c37cd0fba001a4c95a2963ad2e8055c9eaebabe
hieuit06 <lehieu206201@gmail.com>
2022-07-08 21:40:41 +0700

    update

--- home/views.py
+++ home/views.py
@@ -1388,3 +1388,22 @@ def thanhToanLuong(request):
                 "success": True,
                 "message": "Nhận tiền thành công",
             })
+
+def error_404(request, exception=None):
+    # return render(request, 'error_404.html')
+    return JsonResponse({
+        'success': False, 
+        'message': 'Page not found',
+        'error': '404'
+    })
+
+
+def error_500(request):
+    # return render(request, 'error_500.html')
+    return JsonResponse({
+        'success': False,
+        'message': 'Internal server error',
+        'error': '500'
+    })
+
+# 404 Page Not Found
